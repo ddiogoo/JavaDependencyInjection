a9736fb869b552af7d1c0108044b61ccfb380ac6
Diogo Martins de Assis <diogodmda@gmail.com>
2024-11-19 21:02:47 -0300

    feat: Implementing the logger interface

---
+++ java-dependency-injection/src/log/ILogger.java
@@ -0,0 +1,13 @@
+package log;
+
+/**
+ * Represents a logger.
+ */
+public interface ILogger {
+  /**
+   * Logs the specified message.
+   * 
+   * @param message the message to log.
+   */
+  void log(String message);
+}
