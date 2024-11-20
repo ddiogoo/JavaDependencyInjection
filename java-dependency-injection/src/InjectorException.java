19baefa1d2ee209fcb54c44627dcf9d969649d69
Diogo Martins de Assis <diogodmda@gmail.com>
2024-11-19 21:02:20 -0300

    feat: Implementing the exception about the Injector

---
+++ java-dependency-injection/src/InjectorException.java
@@ -0,0 +1,10 @@
+public class InjectorException extends Exception {
+  /**
+   * Constructs a new InjectorException with the specified detail message.
+   * 
+   * @param message the detail message.
+   */
+  public InjectorException(String message) {
+    super(message);
+  }
+}
